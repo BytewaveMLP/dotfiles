4324f7047db772c9279261184b0ad7e7dfb07cd0
Eliot Partridge <shortcircuit369@gmail.com>
2016-06-03 00:08:36 -0500

    More options while installing, yay

--- install.sh
+++ install.sh
@@ -29,17 +29,30 @@ ask() {
 dir=`pwd`
 
 if ask "Install symlink for .zshrc?" Y; then
+	if [ -f "${HOME}/.zshrc" ] ; then
+		if ask ".zshrc already exists. Replace?" Y; then
+			mv ${HOME}/.zshrc ${HOME}/.zshrc.pre-dotfiles
+		fi
+	fi
+
 	ln -s ${dir}/.zshrc ${HOME}/.zshrc
 fi
 
 if ask "Install symlinks for .config/?" Y; then
+	if [ ! -d "${HOME}/.config" ] ; then
+		if ask "~/.config does not exist. Create?" Y; then
+			mkdir ${HOME}/.config/
+		fi
+	fi
+
 	ln -s ${dir}/.config/* ${HOME}/.config/
 fi
 
 if ask "Install symlinks for bin/?" Y; then
 	if [ ! -d "${HOME}/bin/" ] ; then
-		echo "No ~/bin directory. Creating..."
-		mkdir ${HOME}/bin/
+		if ask "~/bin does not exist. Create?" Y; then
+			mkdir ${HOME}/bin/
+		fi
 	fi
 
 	ln -s ${dir}/bin/* ${HOME}/bin/
